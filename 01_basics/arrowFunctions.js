f611305e449cbc4faa5279b5e6d5f1054e43619c
Saurabh Nagmoti <saurabhnagmoti@gmail.com>
2025-11-20 05:15:30 +0000

    learned arrow functions and different types of functions in javascript

---
+++ 01_basics/arrowFunctions.js
@@ -0,0 +1,20 @@
+//way 1 of writing normal function
+function addition(a,b)
+{
+    return a+b
+}
+console.log(addition(3,4))
+
+//way 2 of writing function
+
+const division = function(x,y)
+{
+    return x/y
+}
+console.log(division(4,2))
+
+//way of writing arrow functions
+
+const multiply = (num1,num2) => num1*num2
+//calling arrow function below
+console.log(multiply(9,8));
