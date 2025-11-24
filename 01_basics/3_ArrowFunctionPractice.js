eae9a3107da45a64e40009ae0ad84f8e197e3387
Saurabh Nagmoti <saurabhnagmoti@gmail.com>
2025-11-24 01:26:17 +0000

    practiced Javascript functions

---
+++ 01_basics/3_ArrowFunctionPractice.js
@@ -0,0 +1,16 @@
+function addition(a,b)
+{
+    return a+b;
+}
+console.log(addition(2,4));
+
+//Arrow function 
+const multiply = (a,b) => a*b;
+console.log(3*6)
+
+//normal function
+const division = function(x,y)
+{
+    return x/y
+}
+console.log(division(8,4))
